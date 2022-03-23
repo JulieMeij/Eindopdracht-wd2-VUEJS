d349c58c2d45a856cbb8cb78ab96bb8f819bca45
JulieMeij <54932056+JulieMeij@users.noreply.github.com>
2022-03-23 15:06:13 +0100

    setup2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,43 @@
 <template>
   <section>
     <div class="container">
-      <h2 class="mt-3 mt-lg-5">Welcome to the homepage</h2>
+      <h1 class="py-3">Welcome {{ loggedUser }} </h1>
+      <p>On this website you can play blackjack against the computer.</p>
+      <h2>Rules of blackjack</h2>
+      <p>
+        You need to attempt to beat the dealer by getting a count as close to 21
+        as possible, without going over 21. Face cards are 10 and any other card
+        is its number's value. Aces are either worth 11 if you're under 21 and
+        worth 1 if you would have gone over 21. Pressing HIT means you get a
+        random card. Pressing STAND means it's the dealers turn to get cards.
+      </p>
+      <p>Tip: it's best to STAND if your score is 17 or higher.</p>
+
+      <h4>When do you win?</h4>
+      <ul>
+        <li>
+          If you get 21 you win, unless the dealer gets 21 as well then it's a
+          tie.
+        </li>
+        <li>If you go over 21 you lose.</li>
+        <li>If at the end your score is higher then the dealer's you win.</li>
+        <li>If the dealer goes over 21 you win.</li>
+      </ul>
+
+      <router-link to="/play/blackjack" class="nav-link" active-class="active"> Play blackjack </router-link>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: "Home", 
+  name: "Home",
+  data() {
+    return {
+      loggedUser: "",
+    };
+  },
 };
-
-
 </script>
 
 <style>
